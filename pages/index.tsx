04222e9b7f2773ae67e0af5a2a45f4dd4854acdc
Dr. Derek Austin <derekraustin@gmail.com>
2022-10-08 13:35:00 -0500

    chore(prettier): run Prettier on entire repository

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
+import Head from "next/head"
+import Image from "next/image"
+import Link from "next/link"
 
 export default function Home() {
   return (
