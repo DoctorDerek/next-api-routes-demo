479a2ba7a051e996182fc7867d1ccc5f4496eed5
Dr. Derek Austin <derekraustin@gmail.com>
2022-10-08 13:34:21 -0500

    fix(architecture): remove all the "Next.js defaults" and add a custom `_document.tsx` to support Google font "Inter"

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-rose-300 font-xl">
+      <Head>
+        <title>Next API Routes Demo by @DoctorDerek</title>
+      </Head>
+
+      <main>
+        <h1>h1</h1>
+      </main>
+
+      <footer>
+        Created by{" "}
+        <Link href="https://DoctorDerek.com">
+          <a>@DoctorDerek</a>
+        </Link>
+      </footer>
+    </div>
+  )
+}
